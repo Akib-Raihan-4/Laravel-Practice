aa351d727acb495eeeadddf451c75410965465f0
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-02 16:11:19 -0400

    route parameters are read and also the condition set

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,25 @@ Route::get('/getMethod', function () {
 
 // another way to get view
 // Route::view('/get', 'get');
+
+// Route::get('giveID/{id?}', function (string $id = null) {
+//     if($id){
+//         return view('getID', ['id' => $id]);
+//     }
+//     else{
+//         return "<h1>No ID found</h1>";
+//     }
+// }) -> whereNumber('id');
+
+Route::get('giveName/{name?}', function(string $name = null){
+    if($name){
+        return view('getID', ['id' => $name]);
+    }
+    else{
+        return "<h1>No ID found</h1>";
+    }
+}) -> whereAlpha('name');
+
+// There can be more like "whereAplhaNumeric('name of the parameter given');"
+// and then to specify like it should be movie/song here "whereIn('id', ['movie','song']);"
+// can use regular expression also "where('id', [0-9]+);"
