748858b8ed7c41966a4f631ec06273db613f6591
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-10-04 19:22:19 -0400

    foreach loops properties is learned

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,4 +20,46 @@
 
 {{-- For javaScript Code --}}
 
-{!! "<script>alert('Alert Box')</script>" !!}
+{{-- {!! "<script>alert('Alert Box')</script>" !!} --}}
+
+
+{{-- Writing php code in blade and declaring variable --}}
+
+{{-- @php
+    $user = "Akib Raihan";
+@endphp
+
+{{ $user }} --}}
+
+
+@php
+    $names = ["Akib", "Dorothi", "Himel", "Sigda"]
+@endphp
+
+<ul>
+    @foreach ($names as $name)
+        <li>{{ $name }}</li>
+    @endforeach
+</ul>
+
+
+{{-- Some properties of foreach loops --}}
+{{-- $loop->index, $loop->count, $loop->iteration, $loop->first,$loop->last, $loop->even, $loop->odd and many more--}}
+
+<ul>
+    @foreach ($names as $name )
+        <li> {{ $loop->index }} -- {{ $name }}</li>
+    @endforeach
+</ul>
+
+
+<ul>
+    @foreach ($names as $name )
+        @if ($loop->first)
+            <li style="color:rgb(4, 0, 255)"> {{ $loop->index }} -- {{ $name }}</li>
+        @else
+            <li> {{ $loop->index }} -- {{ $name }}</li>
+        @endif
+
+    @endforeach
+</ul>
